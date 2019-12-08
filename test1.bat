db3d53e252c3c954cc2a458dc654c03feaa76879
ShrutiSinhaa <shruti.sinha@hotmail.com>
2019-12-08 18:50:44 +0530

    thgis is new code

---
+++ test1.bat
@@ -0,0 +1,17 @@
+Create repo - public private
+github.com - create repo
+local system  -create folder /  clone the repository
+add code file
+commit git / uncommit
+push git
+see the updates on remote repo
+changes
+pull 
+
+GIT GUI 
+staging
+commit
+unstage
+history - details 
+versions
+
